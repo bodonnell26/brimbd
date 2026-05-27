994ae3899a58de31ca3963f17ce269b24f01ff52
bodonnell26 <bodonnell126@gmail.com>
2026-05-27 18:42:11 -0400

    Begin development

---
+++ components/Navigation.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+            <div className="brimdb-logo">BriMDb</div>
+        </header>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import { useState } from 'react'
+import Navigation from '../components/Navigation'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  function addMovie() {
-    console.log("added!")
+  function searchMovie() {
+    console.log("searching!")
   }
+  function handleSubmit(e) {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const titleSearch = formData.get("title")
+    console.log(titleSearch);
+  }
+  
   return (
     <>
-      <section className='addMovie'>  
-        <h2>Add a new movie</h2>
-        <form>
-          Title: <input type="text" name="title"></input>
-          <button onClick={addMovie}>Add Movie</button>
-        </form>
+      <Navigation />
+      <section className='addMovie'>
+        <div className="container">
+          <h2>Add a new movie</h2>
+          <form onSubmit={handleSubmit}>
+            Title: <input type="text" name="title"></input>
+            <button onClick={searchMovie}>Add Movie</button>
+          </form>
+        </div> 
       </section>
       <section className='currentLibrary'>
         
